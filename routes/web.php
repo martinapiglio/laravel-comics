dd467b605b25358504c8b715ac9ddae55a44d8d6
martinapiglio <martina.piglio@gmail.com>
2023-05-09 14:42:25 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // config() used to access comics.php file in config directory
+    $comics = config('comics');
+
+    return view('home', compact('comics'));
+})->name('home');
