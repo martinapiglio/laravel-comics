ebed592ad79719b46bdb82aaeebb9f3028610329
martinapiglio <martina.piglio@gmail.com>
2023-05-09 14:47:27 +0200

    layout + partials folders + homepage created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts/app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel Comics</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
+@section('content')
     <ul>
         @foreach ($comics as $comic)
             <li>
@@ -24,6 +8,7 @@
             </li>        
         @endforeach
     </ul>
+@endsection
 
 </body>
 
